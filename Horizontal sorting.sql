de88a88792c052cae7c504b71c560ae2cddaa204
Aman Chandra <aman05chandra2018@gmail.com>
2024-06-22 15:25:43 +0530

    Create Horizontal sorting.sql
    https://www.youtube.com/watch?v=FZm7NgybHWA

---
+++ Horizontal sorting.sql
@@ -0,0 +1,30 @@
+
+
+
+CREATE TABLE subscriber (
+ sms_date date ,
+ sender varchar(20) ,
+ receiver varchar(20) ,
+ sms_no int
+);
+-- insert some values
+INSERT INTO subscriber VALUES ('2020-4-1', 'Avinash', 'Vibhor',10);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Vibhor', 'Avinash',20);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Avinash', 'Pawan',30);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Pawan', 'Avinash',20);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Vibhor', 'Pawan',5);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Pawan', 'Vibhor',8);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Vibhor', 'Deepak',50);
+
+
+with cte as(
+select *,case when receiver>sender then sender
+else receiver end as senders,
+case when receiver>sender then receiver
+else sender end as receivers
+from subscriber
+)
+select senders,receivers,sum(sms_no) as total_sms
+from cte
+group by  senders,receivers
+order by sum(sms_no)
